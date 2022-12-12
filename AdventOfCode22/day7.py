7b2d1b072c9a87e89ba0ff5f33ec0d5d91d36201
Dimitar Markov <dimitar.markov@sap.com>
2022-12-12 15:50:00 +0200

    Refactor day7

--- AdventOfCode22/day7.py
+++ AdventOfCode22/day7.py
@@ -28,60 +28,144 @@ current_tree_key = ""
 #     'C': [],
 # }
 
+class FsItem:
+    def __init__(self, name):
+        self.name = name
+        
+    def size():
+        return 0
+      
+class RootFolder(FsItem):
+    def __init__(self, name):
+        self.name = name   
+        self.children = []
+        self.__name = "/" 
+    
+    def size(self):
+        return sum([child.size() for child in self.children])
+        
+class Folder(FsItem):
+    def __init__(self, name, parent):   
+        self.name = name
+        self.parent = parent
+        self.children = []
+    
+    def size(self):
+        return sum([child.size() for child in self.children])
+
+
+class File(FsItem):
+    def __init__(self, name, size, parent):
+        self.name = name
+        self.__size = size
+         
+    def size(self):
+        return self.__size
+
+
+root = RootFolder("root")
+current_folder = root
+print(root)
+
+
+
 with open("/Users/dmarkov/Projects/zeRepo/AdventOfCode22/day7_input.txt", "r") as f:
     lines = f.readlines()
     
 for line in lines:
-    line = line.strip().split(" ")
+    line = line.strip()
     print(line)
-    if line[0] == "$":
-        params = line[1:]
-        handle_command(params)
-        print("Handle command")
-    elif line[0] == "dir":
-        tree[current_tree_key].append(line[1])
-    else:
-        file_dict = {
-            "name": line[1],
-            "size": line[0]
-        }
-        tree[current_tree_key].append(file_dict)
-    
-current_dir = []
-
-print(tree)
-result_list = {}
-def calc_dir_size(dir, tree):
-    sizes_dict = {}
-    size = 0
-    for item in tree[dir]:
-        if type(item) == str and item not in sizes_dict:
-            size = calc_dir_size(item, tree)
-        elif type(item) == str and item in sizes_dict:
-            size += sizes_dict[item]
+    if line == "$ cd /" or line == "$ ls":
+        continue
+    if line == "$ ls":
+        continue
+    if line == "$ cd ..":
+        if isinstance(current_folder, RootFolder):
+            raise Exception("Cannot go up from root")
+        else: 
+            current_folder = current_folder.parent
+            continue
+    if line.startswith("$ cd"):
+        folder_name = line.split(" ")[-1]
+        try:
+            folder_index = [folder.name for folder in current_folder.children].index(folder_name)
+        except ValueError:
+            folder_index = None
+        if folder_index:
+            folder = current_folder.children[folder_index]
         else:
-            size += int(item["size"])
-    result_list[dir] = size
-    return size
+            folder = Folder(folder_name, current_folder)
+            current_folder.children.append(folder)
+        current_folder = folder
+    else:
+        #process file 
+        parts = line.split(" ")
+        file_name = parts[1]
+        try:
+            file_index = [item.name for item in current_folder.children].index(file_name)
+        except ValueError:
+            file_index = None
+        if not file_index:
+            if parts[0] == "dir":
+                child = Folder(file_name, current_folder)
+                current_folder.children.append(child)
+            else:
+                size = int(parts[0])
+                child = File(file_name, size, current_folder)
+                current_folder.children.append(child)
 
-def calc_tree_size(tree):
-    total_size = 0
-    for item in tree:
-        print(item)
-        calc_dir_size(item, tree)
-    #     if type(item) == str:
-    #         total_size += 1
-    #     else:
-    #         total_size += int(item["size"])
-    # return total_size
+
+used_space = root.size()
+needed_space = 70000000 - used_space
+print(used_space, needed_space)
+
+queue = [root]
+result = 0
+
+while len(queue) > 0:
+    item = queue.pop(0)
+    size = item.size()
+    if (size <= 100000):
+        result += size
+    for child in item.children:
+        if isinstance(child, Folder):
+            queue.append(child)
 
 
-calc_dir_size("/" , tree)
-print(result_list)
+print(result)
+# print(tree)
+# result_list = {}
+# def calc_dir_size(dir, tree):
+#     sizes_dict = {}
+#     size = 0
+#     for item in tree[dir]:
+#         if type(item) == str and item not in sizes_dict:
+#             size = calc_dir_size(item, tree)
+#         elif type(item) == str and item in sizes_dict:
+#             size += sizes_dict[item]
+#         else:
+#             size += int(item["size"])
+#     result_list[dir] = size
+#     return size
 
-total = 0
-for dir in result_list:
-    if result_list[dir] < 100000:
-        total += result_list[dir]
+# def calc_tree_size(tree):
+#     total_size = 0
+#     for item in tree:
+#         print(item)
+#         calc_dir_size(item, tree)
+#     #     if type(item) == str:
+#     #         total_size += 1
+#     #     else:
+#     #         total_size += int(item["size"])
+#     # return total_size
 
-print(total)
+
+# calc_dir_size("/" , tree)
+# print(result_list)
+
+# total = 0
+# for dir in result_list:
+#     if result_list[dir] < 100000:
+#         total += result_list[dir]
+
+# print(total)
